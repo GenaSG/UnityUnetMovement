792221a755d53cd5e19e1789bbae6a72dde749a8
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2015-09-04 15:50:54 +0200

    basic project setup

---
+++ Scripts/Spectator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class Spectator : NetworkBehaviour {
+	public GameObject playerPrefab;
+
+	public void Spawn(){
+		if (isLocalPlayer) {
+			Cmd_Spawn();
+		}
+	}
+	[Command]
+	void Cmd_Spawn(){
+		if (hasAuthority) {
+			Transform spawn = NetworkManager.singleton.GetStartPosition ();
+			GameObject player = (GameObject)Instantiate (playerPrefab, spawn.position, spawn.rotation);
+			NetworkServer.Destroy (this.gameObject);
+			NetworkServer.ReplacePlayerForConnection (this.connectionToClient, player, this.playerControllerId);
+		}
+	}
+
+}
