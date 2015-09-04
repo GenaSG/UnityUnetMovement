04f402f157168063df19eb0a156ba7319b81f672
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2015-09-04 18:18:59 +0200

    initial animationController

---
+++ Scripts/AnimationController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+	public Animator _animator;
+	public NetworkPawn _networkPawn;
+	public Vector3 _lastPosition;
+	public CharacterController _characterController;
+	private Vector3 _velocity;
+	// Use this for initialization
+	void Start () {
+		_lastPosition = _networkPawn.pawn.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_velocity = Vector3.Lerp (_velocity, _characterController.velocity, 10 * Time.deltaTime);
+
+		_animator.SetFloat ("forward", _velocity.z);
+		_animator.SetFloat ("sides",_velocity.x);
+	}
+}
--- Scripts/BodyController.cs
+++ Scripts/BodyController.cs
@@ -2,29 +2,25 @@
 using System.Collections;
 
 public class BodyController : MonoBehaviour {
-	public Animator animator;
-	public NetworkPawn networkPawn;
-	private Transform head;
-	private Transform spine;
-	private Transform chest;
+	public Animator _animator;
+	public NetworkPawn _networkPawn;
+	private Transform _head;
+	private Transform _spine;
+	private Transform _chest;
 	// Use this for initialization
 	void Start () {
-		head = animator.GetBoneTransform (HumanBodyBones.Head);
-		spine = animator.GetBoneTransform (HumanBodyBones.Spine);
-		chest = animator.GetBoneTransform (HumanBodyBones.Chest);
+		_head = _animator.GetBoneTransform (HumanBodyBones.Head);
+		_spine = _animator.GetBoneTransform (HumanBodyBones.Spine);
+		_chest = _animator.GetBoneTransform (HumanBodyBones.Chest);
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		Quaternion targetRotation = Quaternion.Euler(networkPawn.pawnRotation.eulerAngles.x + networkPawn.pawn.eulerAngles.x,networkPawn.pawn.eulerAngles.y,networkPawn.pawn.eulerAngles.z);
+		Quaternion targetRotation = Quaternion.Euler(_networkPawn.pawnRotation.eulerAngles.x + _networkPawn.pawn.eulerAngles.x,_networkPawn.pawn.eulerAngles.y,_networkPawn.pawn.eulerAngles.z);
 		//Getting delta rotations
-		Quaternion deltaSpineRot = Quaternion.Inverse (networkPawn.pawn.rotation) * spine.rotation;
-		Quaternion deltaChestRot = Quaternion.Inverse (networkPawn.pawn.rotation) * chest.rotation;
-		spine.rotation = Quaternion.Slerp(spine.rotation,targetRotation,0.3f);
-		spine.localRotation *= deltaSpineRot;
-		chest.rotation = Quaternion.Slerp(chest.rotation,targetRotation,0.3f);
-		chest.localRotation *= deltaChestRot;
+		_spine.rotation = Quaternion.Slerp(_spine.rotation,targetRotation,0.3f);
+		_chest.rotation = Quaternion.Slerp(_chest.rotation,targetRotation,0.3f);
 
-		head.rotation = targetRotation;
+		_head.rotation = targetRotation;
 	}
 }
