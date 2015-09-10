eae119ec76b8b507bbb267e1d00bf467664a5150
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2015-09-10 21:09:06 +0200

    initial item pickup

---
+++ Scripts/Inventory.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+
+public class Inventory : NetworkBehaviour {
+	public List<GameObject> items = new List<GameObject>();
+
+	public void GiveItem(GameObject item){
+		if (hasAuthority) {
+			Debug.Log("Spawn item");
+			GameObject newItem = (GameObject)Instantiate(item);
+			NetworkServer.Spawn(newItem);
+			Rpc_RegistreItem(newItem.GetComponent<NetworkIdentity>().netId);
+			items.Add(newItem);
+		}
+	}
+	[ClientRpc]
+	void Rpc_RegistreItem(NetworkInstanceId id){
+		if (!hasAuthority) {
+			Debug.Log("Pistol ID = " + id.ToString());
+			GameObject item = ClientScene.FindLocalObject (id);
+			items.Add(item);
+		}
+	}
+}
---
+++ Scripts/ItemScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class ItemScript : NetworkBehaviour {
+	public GameObject actualItem;
+
+	void OnTriggerEnter(Collider other){
+		if (hasAuthority) {
+			other.SendMessageUpwards("GiveItem",actualItem,SendMessageOptions.DontRequireReceiver);
+		}
+	}
+}
