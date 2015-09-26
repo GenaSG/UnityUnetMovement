abf5ca3a3854dab144cfa2f2d22622eaba864dc6
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2015-09-26 20:09:14 +0200

    Special reference list component added to use with StateMachineBehaviours

---
+++ Scripts/ComponentsList.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ComponentsList : MonoBehaviour {
+	public Inventory inventory;
+	public BodyController bodyController;
+	public NetworkPawn networkPawn;
+}
