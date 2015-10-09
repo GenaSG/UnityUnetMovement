139ca9079dac12153cdcd73bad9e6b5eea9884b2
Gennadiy Shvetsov <gennadiy.shvetsov@gmail.com>
2015-10-09 17:32:38 +0200

    cosmetics

---
+++ Scripts/AutoDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDestroy : MonoBehaviour {
+	private ParticleSystem _particleSystem;
+	// Use this for initialization
+	void Start () {
+		_particleSystem = GetComponent<ParticleSystem> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!_particleSystem.IsAlive ()) {
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
